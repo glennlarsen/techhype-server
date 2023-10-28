7eaa562b8708a0b1b731983b7cb1562350bcdd14
Glenn Larsen <50579947+glennlarsen@users.noreply.github.com>
2023-10-28 20:58:35 +0200

    nodemailer secure true

--- routes/middleware/nodemailer.js
+++ routes/middleware/nodemailer.js
@@ -3,7 +3,7 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
     host: process.env.NODEMAILER_HOST,
     port: process.env.NODEMAILER_PORT,
-    secure: false, // true for 465, false for other ports
+    secure: true, // true for 465, false for other ports
     auth: {
       user: process.env.NODEMAILER_USER,
       pass: process.env.NODEMAILER_PASS,
