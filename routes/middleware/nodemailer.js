291b3f9e1c098f3b39f224f2b266fbdf88fd6842
Glenn Larsen <50579947+glennlarsen@users.noreply.github.com>
2023-10-28 21:06:39 +0200

    reset nodemailer cred

--- routes/middleware/nodemailer.js
+++ routes/middleware/nodemailer.js
@@ -1,9 +1,9 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-    host: process.env.NODEMAILER_HOST,
-    port: process.env.NODEMAILER_PORT,
-    secure: true, // true for 465, false for other ports
+    host: "smtp.titan.email",
+    port: 587,
+    secure: false, // true for 465, false for other ports
     auth: {
       user: process.env.NODEMAILER_USER,
       pass: process.env.NODEMAILER_PASS,
