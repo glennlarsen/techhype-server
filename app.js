33fecad331c6f38168a9892b61d1540286476287
Glenn Larsen <50579947+glennlarsen@users.noreply.github.com>
2024-05-09 12:26:11 +0200

    updated CORS credentials

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ const corsOptions = {
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   preflightContinue: false,
   optionsSuccessStatus: 204,
+  credentials: true,
 };
 
 app.use(cors(corsOptions));
