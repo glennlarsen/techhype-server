a686b2f770cd36eca85b6f44d6d8bd9d8e96d7c5
Glenn Larsen <50579947+glennlarsen@users.noreply.github.com>
2024-05-09 17:54:33 +0200

    samsite none

--- routes/auth.js
+++ routes/auth.js
@@ -89,12 +89,12 @@ router.post("/login", authLimiter, jsonParser, async (req, res, next) => {
       res.cookie('token', token, {
         httpOnly: true,
         secure: true, //true in production only
-        sameSite: 'lax', // can be 'strict' or 'lax or None',
+        sameSite: 'None', // can be 'strict' or 'lax or None',
       });
       res.cookie('refreshToken', refreshToken, {
         httpOnly: true,
         secure: true,
-        sameSite: 'lax',
+        sameSite: 'None',
       });
 
       return res.jsend.success({
