8d2294c5acd9de4596a5db104b7df6db2148c26d
Glenn Larsen <50579947+glennlarsen@users.noreply.github.com>
2024-07-28 15:23:51 +0200

    remove google strategy passport

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require("dotenv").config({ path: `.env.${process.env.NODE_ENV}` });
 console.log(`Environment: ${process.env.NODE_ENV}`);
 console.log(`Database Host: ${process.env.HOST}`);
+
 var createError = require("http-errors");
 var express = require("express");
 const cors = require("cors");
@@ -11,19 +12,25 @@ var db = require("./models");
 const passport = require('passport');
 require('./config/passport-config');
 
-
-db.sequelize.sync({ force: false });
 const swaggerUi = require("swagger-ui-express");
 const swaggerFile = require("./swagger-output.json");
 const bodyParser = require("body-parser");
+
 var indexRouter = require("./routes/index");
 var authRouter = require("./routes/auth");
 var usersRouter = require("./routes/users");
 var cardsRouter = require("./routes/cards");
-var AddressRouter = require("./routes/address");
-var WorkInfoRouter = require("./routes/workInfo");
-var SocialMediaRouter = require("./routes/socialMedia");
-console.log("environment: ", process.env.NODE_ENV);
+var addressRouter = require("./routes/address");
+var workInfoRouter = require("./routes/workInfo");
+var socialMediaRouter = require("./routes/socialMedia");
+
+db.sequelize.sync({ force: false })
+  .then(() => {
+    console.log("Database synchronized");
+  })
+  .catch((err) => {
+    console.error("Database synchronization error:", err);
+  });
 
 var app = express();
 
@@ -46,13 +53,6 @@ const corsOptions = {
   credentials: true,
 };
 
-
-
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-
-app.use(bodyParser.json());
 app.use(cors(corsOptions));
 app.use(logger("dev"));
 app.use(express.json());
@@ -60,33 +60,28 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use(bodyParser.json());
 
 app.use("/", indexRouter);
 app.use("/auth", authRouter);
 app.use("/users", usersRouter);
 app.use("/cards", cardsRouter);
-app.use("/address", AddressRouter);
-app.use("/workInfo", WorkInfoRouter);
-app.use("/socialMedia", SocialMediaRouter);
-
+app.use("/address", addressRouter);
+app.use("/workInfo", workInfoRouter);
+app.use("/socialMedia", socialMediaRouter);
 
 app.use("/doc", swaggerUi.serve, swaggerUi.setup(swaggerFile));
-
-
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
-
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render("error");
 });
